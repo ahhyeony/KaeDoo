48f17582ddbc19951b9b30419a2adab17a8ec59b
ahhyeon <ahhyeon27@gmail.com>
2024-06-09 05:26:28 +0900

    졸음 체크 로직 수정

--- lib/common/widget/w_face_detector.dart
+++ lib/common/widget/w_face_detector.dart
@@ -110,12 +110,12 @@ class FaceDetectionState extends State<FaceDetection> {
       print("얼굴 위치: ${face.boundingBox}");
     }
 
-    if (inputImage.metadata?.size != null && inputImage.metadata?.rotation != null) {
+    bool allEyesClosed = faces.isEmpty; // 초기값을 얼굴이 감지되지 않은 상태로 설정
+
+    if (faces.isNotEmpty) {
       // eye detection
       List<Rect> leftEyeRects = [];
       List<Rect> rightEyeRects = [];
-
-      //bool allEyesClosed = faces.isNotEmpty; // 초기값을 얼굴이 감지된 상태로 설정
 
       for (Face face in faces) {
         if (face.leftEyeOpenProbability != null && face.rightEyeOpenProbability != null) {
@@ -126,9 +126,9 @@ class FaceDetectionState extends State<FaceDetection> {
           bool rightEyeClosed = face.rightEyeOpenProbability! < 0.5;
 
           // 한 사람이라도 눈을 뜨고 있으면 졸음 상태가 아님
-          // if (!leftEyeClosed || !rightEyeClosed) {
-          //   allEyesClosed = false;
-          // }
+          if (!leftEyeClosed || !rightEyeClosed) {
+            allEyesClosed = false;
+          }
 
           setState(() {
             isLeftEyeOpen = !leftEyeClosed;
@@ -140,51 +140,19 @@ class FaceDetectionState extends State<FaceDetection> {
 
           if (leftEyePos != null) {
             leftEyeRects.add(Rect.fromCenter(
-              center: Offset(leftEyePos.x.toDouble(), leftEyePos.y.toDouble()), // 변환 적용
-              width: 20.0, // 변환 적용
-              height: 20.0, // 변환 적용
+              center: Offset(leftEyePos.x.toDouble(), leftEyePos.y.toDouble()),
+              width: 20.0,
+              height: 20.0,
             ));
           }
 
           if (rightEyePos != null) {
             rightEyeRects.add(Rect.fromCenter(
-              center: Offset(rightEyePos.x.toDouble(), rightEyePos.y.toDouble()), // 변환 적용
-              width: 20.0, // 변환 적용
-              height: 20.0, // 변환 적용
+              center: Offset(rightEyePos.x.toDouble(), rightEyePos.y.toDouble()),
+              width: 20.0,
+              height: 20.0,
             ));
           }
-        }
-      }
-
-      //if (allEyesClosed && CTimeStorage().isTimerRunning()) {
-      if (CTimeStorage().isTimerRunning()) {
-        if (_drowsinessTimer == null) {
-          print("눈이 감겼습니다. 타이머를 시작합니다.");
-          _drowsinessTimer = Timer.periodic(Duration(seconds: 1), (timer) {
-            _closedEyeDuration += 1;
-            print("눈을 감은 시간: $_closedEyeDuration 초");
-
-            if (_closedEyeDuration == _drowsinessThreshold) {
-              print('졸음 상태입니다! 눈을 $_drowsinessThreshold 초 이상 감고 있습니다.');
-              _isDrowsy = true;
-            }
-          });
-        }
-      } else {
-        if (_isDrowsy && _closedEyeDuration > _drowsinessThreshold) {
-          // 눈을 감았다가 다시 뜨면 기록
-          int additionalSleepDuration = _closedEyeDuration - _drowsinessThreshold;
-          print("눈을 다시 뜨면 감긴 시간을 기록합니다: $additionalSleepDuration 초");
-          CTimeStorage().addSleepDuration(Duration(seconds: additionalSleepDuration));
-        }
-
-        // 초기화
-        _closedEyeDuration = 0;
-        _isDrowsy = false;
-        if (_drowsinessTimer != null) {
-          print("타이머를 취소합니다.");
-          _drowsinessTimer?.cancel();
-          _drowsinessTimer = null;
         }
       }
 
@@ -193,24 +161,49 @@ class FaceDetectionState extends State<FaceDetection> {
         _rightEyeRects = rightEyeRects;
         isLeftEyeOpen = faces.any((face) => face.leftEyeOpenProbability! > 0.5);
         isRightEyeOpen = faces.any((face) => face.rightEyeOpenProbability! > 0.5);
+        _faceCount = faces.length;
+
+        print('Left Eye: ${isLeftEyeOpen ? 'Open' : 'Closed'}');
+        print('Right Eye: ${isRightEyeOpen ? 'Open' : 'Closed'}');
       });
-
-      print('Left Eye: ${isLeftEyeOpen ? 'Open' : 'Closed'}');
-      print('Right Eye: ${isRightEyeOpen ? 'Open' : 'Closed'}');
-
-      _faceCount = faces.length; // 감지된 얼굴 수 업데이트
-      print("감지된 얼굴 수 업데이트: $_faceCount");
     } else {
-      _faceCount = 0;
-      _leftEyeRects = [];
-      _rightEyeRects = [];
-      _closedEyeDuration = 0; // 얼굴이 감지되지 않으면 시간 초기화
-      _isDrowsy = false;
-      print("메타데이터가 없음. _faceCount를 0으로 설정");
+      // 얼굴이 감지되지 않는 경우에도 졸음 상태를 유지하도록 합니다.
+      setState(() {
+        _faceCount = 0;
+      });
+      allEyesClosed = true; // 얼굴이 감지되지 않는 경우 모든 눈이 감긴 상태로 가정
     }
-    if (mounted) {
-      setState(() {});
-      print("----------------------------- 상태가 설정되었습니다.");
+
+    // 졸음 체크 로직
+    if (allEyesClosed) {
+      if (_drowsinessTimer == null) {
+        print("눈이 감겼습니다. 타이머를 시작합니다.");
+        _drowsinessTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+          _closedEyeDuration += 1;
+          print("눈을 감은 시간: $_closedEyeDuration 초");
+
+          if (_closedEyeDuration == _drowsinessThreshold) {
+            print('졸음 상태입니다! 눈을 $_drowsinessThreshold 초 이상 감고 있습니다.');
+            _isDrowsy = true;
+          }
+        });
+      }
+    } else {
+      if (_isDrowsy && _closedEyeDuration > _drowsinessThreshold) {
+        // 눈을 감았다가 다시 뜨면 기록
+        int additionalSleepDuration = _closedEyeDuration - _drowsinessThreshold;
+        print("눈을 다시 뜨면 감긴 시간을 기록합니다: $additionalSleepDuration 초");
+        CTimeStorage().addSleepDuration(Duration(seconds: additionalSleepDuration));
+      }
+
+      // 초기화
+      _closedEyeDuration = 0;
+      _isDrowsy = false;
+      if (_drowsinessTimer != null) {
+        print("타이머를 취소합니다.");
+        _drowsinessTimer?.cancel();
+        _drowsinessTimer = null;
+      }
     }
   }
 
